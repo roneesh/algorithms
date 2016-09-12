1846507d93678618707033408f9496c78f66b25b
Roneesh <roneesh@gmail.com>
2016-09-12 15:13:41 -0500

    update to factorial

--- factorial.js
+++ factorial.js
@@ -1,11 +1,6 @@
 // Implementing recursive factoring in one line with a ternary
+
 function factorial(n) {
 	return (n === 1) ? n : n * factorial(n - 1);
 }
 console.log(factorial(30))
-
-// Attaching it to the Math object for fun
-Math.factorial = function(n) {
-	return (n === 1) ? n : n * factorial(n - 1);
-}
-console.log(Math.factorial(30))
